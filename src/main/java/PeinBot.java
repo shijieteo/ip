129c011d4325050609ae4db8e13acb7d70a06dbb
Shi Jie <shijieteo.sj@gmail.com>
2026-08-18 23:44:40 +0800

    Add greeting and exit message

--- src/main/java/PeinBot.java
+++ src/main/java/PeinBot.java
@@ -1,11 +1,20 @@
 public class PeinBot {
     public static void main(String[] args) {
+
         String banner = "__________       .__      ___.           __   \n"
                 + "\\______   \\ ____ |__| ____\\_ |__   _____/  |_ \n"
                 + " |     ___// __ \\|  |/    \\| __ \\ /  _ \\   __\\\n"
                 + " |    |   \\  ___/|  |   |  \\ \\_\\ (  <_> )  |  \n"
                 + " |____|    \\___  >__|___|  /___  /\\____/|__|  \n"
                 + "               \\/        \\/    \\/             \n";
+
+        String horizontalLine = "_____________________________________________________________";
+        System.out.println(horizontalLine);
         System.out.println(banner);
+        System.out.println("Hello! I'm PeinBot :)");
+        System.out.println("What can I do for you? ");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. See you soon :(");
+        System.out.println(horizontalLine);
     }
 }
