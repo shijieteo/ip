1ceeacec3b7455320e582a03035ecbb684edfa2b
Shi Jie <shijieteo.sj@gmail.com>
2026-08-19 00:02:08 +0800

    Add function to read user input

--- src/main/java/PeinBot.java
+++ src/main/java/PeinBot.java
@@ -1,4 +1,9 @@
+import java.util.Scanner;
+
+
 public class PeinBot {
+    static String HORIZONTAL_LINE = "_____________________________________________________________";
+
     public static void main(String[] args) {
 
         String banner = "__________       .__      ___.           __   \n"
@@ -8,13 +13,25 @@ public class PeinBot {
                 + " |____|    \\___  >__|___|  /___  /\\____/|__|  \n"
                 + "               \\/        \\/    \\/             \n";
 
-        String horizontalLine = "_____________________________________________________________";
-        System.out.println(horizontalLine);
+        System.out.println(HORIZONTAL_LINE);
         System.out.println(banner);
         System.out.println("Hello! I'm PeinBot :)");
-        System.out.println("What can I do for you? ");
-        System.out.println(horizontalLine);
+
+        String userInput = readInput();
+        processInput(userInput);
+
         System.out.println("Bye. See you soon :(");
-        System.out.println(horizontalLine);
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    public static void processInput(String message) {
+
+    }
+
+    public static String readInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What can I do for you? ");
+        System.out.println(HORIZONTAL_LINE);
+        return scanner.nextLine();
     }
 }
