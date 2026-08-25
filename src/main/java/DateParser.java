5df9de580a20c470ecbf805119a8405bcd4d000e
Shi Jie <shijieteo.sj@gmail.com>
2026-08-25 16:46:15 +0800

    Add DateParser class

---
+++ src/main/java/DateParser.java
@@ -0,0 +1,46 @@
+import javax.swing.text.html.Option;
+import java.time.LocalDate;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.Temporal;
+import java.util.List;
+import java.util.Optional;
+
+public class DateParser {
+    private List<String> dateFormatList;
+    private List<String> dateTimeFormatList;
+
+    DateParser() {
+        this.dateFormatList = List.<String>of("dd-MM-yyyy", "dd/MM/yyyy", "yyyy-MM-dd", "yyyy/MM/dd");
+        this.dateTimeFormatList = List.<String>of("dd-MM-yyyy HH:mm:ss", "dd/MM/yyyy HH:mm:ss",
+                "yyyy-MM-dd HH:mm:ss", "yyyy/MM/dd HH:mm:ss");
+    }
+
+    public Optional<Temporal> parseDate(String userInput) {
+        for(String format : dateFormatList){
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+            try{
+                LocalDate date = LocalDate.parse(userInput, formatter);
+                return Optional.of(date);
+            } catch (DateTimeParseException e) {
+                continue;
+            }
+        }
+        return Optional.<Temporal>empty();
+    }
+
+    public Optional<Temporal> parseDateTime(String userInput) {
+        for(String format : dateTimeFormatList){
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
+            try{
+                LocalDateTime date = LocalDateTime.parse(userInput, formatter);
+                return Optional.<Temporal>of(date);
+            } catch (DateTimeParseException e) {
+                continue;
+            }
+        }
+        return Optional.<Temporal>empty();
+    }
+}
