68f597e45b0c6862d684b63fd25d7c34fd0c15ec
Shi Jie <shijieteo.sj@gmail.com>
2026-08-28 01:01:15 +0800

    Add JavaDoc comments for Parser classes

--- src/main/java/peinbot/parser/DateParser.java
+++ src/main/java/peinbot/parser/DateParser.java
@@ -8,16 +8,31 @@ import java.time.temporal.Temporal;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Parses user input strings to identify date or dateTime values
+ * DateParser contains a set of date and dateTime formats that it accepts
+ */
 public class DateParser {
     private List<String> dateFormatList;
     private List<String> dateTimeFormatList;
 
+    /**
+     * Constructs a DateParser object
+     * Initializes the formats list to include all the date and dateTime formats it can parse
+     */
     public DateParser() {
         dateFormatList = List.<String>of("dd-MM-yyyy", "dd/MM/yyyy", "yyyy-MM-dd", "yyyy/MM/dd");
         dateTimeFormatList = List.<String>of("dd-MM-yyyy HH:mm:ss", "dd/MM/yyyy HH:mm:ss",
                 "yyyy-MM-dd HH:mm:ss", "yyyy/MM/dd HH:mm:ss");
     }
 
+    /**
+     * Converts a user input string into a {@link LocalDate} object
+     *
+     * @param userInput user input containing a date
+     * @return an {@link Optional} containing the date object represented by user input
+     *                  or an empty {@link Optional} if the user input is of an unsupported format
+     */
     public Optional<Temporal> parseDate(String userInput) {
         for (String format : dateFormatList) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
@@ -31,6 +46,13 @@ public class DateParser {
         return Optional.<Temporal>empty();
     }
 
+    /**
+     * Converts a user input string into a {@link LocalDateTime} object
+     *
+     * @param userInput user input containing a date
+     * @return an {@link Optional} containing the dateTime object represented by user input
+     *                  or an empty {@link Optional} if the user input is of an unsupported format
+     */
     public Optional<Temporal> parseDateTime(String userInput) {
         for (String format : dateTimeFormatList) {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern(format);
