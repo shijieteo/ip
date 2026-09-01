2240efb46c61b182975ad6b37c6e1563a3822446
Shi Jie <shijieteo.sj@gmail.com>
2026-09-01 14:12:20 +0800

    Add controller GuiWindow

--- src/main/java/peinbot/ui/controller/GuiWindow.java
+++ src/main/java/peinbot/ui/controller/GuiWindow.java
@@ -1,4 +1,48 @@
 package peinbot.ui.controller;
 
-public class GuiWindow {
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import peinbot.PeinBot;
+import peinbot.ui.view.DialogBox;
+
+public class GuiWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private PeinBot peinBot;
+
+    private Image peinImage = new Image(this.getClass().getResourceAsStream("/images/peinbot.jpg"));
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/user.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    public void setPeinBot(PeinBot peinBot) {
+        this.peinBot = peinBot;
+    }
+
+    @FXML
+    private void handleUserInput() {
+        String userInputText = userInput.getText();
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userInputText, userImage),
+                DialogBox.getPeinBotDialog(peinBot.getResponse(userInputText), peinImage);
+        )
+        userInput.clear();
+    }
+
+
 }
