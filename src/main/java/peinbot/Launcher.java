178cdbe3d80dea4b1fdfdd92f8300dd50ab09146
Shi Jie <shijieteo.sj@gmail.com>
2026-09-01 15:03:10 +0800

    Add main method to Launcher class

--- src/main/java/peinbot/Launcher.java
+++ src/main/java/peinbot/Launcher.java
@@ -1,4 +1,11 @@
-package peinbot;
+import javafx.application.Application;
+import peinbot.ui.GuiMain;
 
+/**
+ * A launcher class to workaround classpath issues.
+ */
 public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(GuiMain.class, args);
+    }
 }
